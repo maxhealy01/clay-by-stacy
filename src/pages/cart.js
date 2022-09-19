9f184af313378956de161d9dee76403f0d73f5b5
Max Healy <maxhealy01@gmail.com>
2022-09-18 20:26:27 -0500

    Tried to get rid of gatsby-plugin-use-shopping-cart entirely

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -5,11 +5,28 @@ import Seo from "../components/seo"
 
 import CartOverview from "../components/Cart/CartOverview"
 
+import { loadStripe } from "@stripe/stripe-js"
+import { CartProvider } from "use-shopping-cart"
+
+const stripePromise = loadStripe(
+  process.env.GATSBY_STRIPE_PUBLISHABLE_KEY
+).toString()
+
 const Cart = () => {
   return (
     <Layout>
-      <Seo title="Cart" />
-      <CartOverview />
+      <CartProvider
+        mode="client-only"
+        stripe={stripePromise}
+        successUrl={`${window.location.origin}/page-2/`}
+        cancelUrl={`${window.location.origin}/`}
+        currency="USD"
+        allowedCountries={["US", "GB", "CA"]}
+        billingAddressCollection={true}
+      >
+        <Seo title="Cart" />
+        <CartOverview />
+      </CartProvider>
     </Layout>
   )
 }
