9ab41eed9bafb127fc3032e74e2ec3f39b8e7fce
Max Healy <maxhealy01@gmail.com>
2022-10-09 21:48:13 -0500

    Filler commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,7 @@
 // const React = require("react")
 // const { CartProvider } = require("use-shopping-cart")
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // exports.wrapRootElement = ({ element }) => {
 //   return (
 //     <CartProvider
@@ -25,25 +25,25 @@
 //     </CartProvider>
 //   )
 // }
-=======
-import React from "react"
-import { Provider } from "react-redux"
-import { CartProvider } from "use-shopping-cart"
+// =======
+// import React from "react"
+// import { Provider } from "react-redux"
+// import { CartProvider } from "use-shopping-cart"
 
-export const wrapRootElement = ({ element }) => {
-  return (
-    <CartProvider
-      mode="payment"
-      cartMode="client-only"
-      stripe={`${process.env.GATSBY_STRIPE_SECRET_KEY}`}
-      successUrl="https://claybystacy.net/success"
-      cancelUrl="https://claybystacy.net/"
-      currency="USD"
-      allowedCountries={["US", "GB", "CA"]}
-      billingAddressCollection={true}
-    >
-      {element}
-    </CartProvider>
-  )
-}
->>>>>>> parent of 35785f4 (Got rid of gatsby-plugin-use-shopping-cart, created a wrapper component instead)
+// export const wrapRootElement = ({ element }) => {
+//   return (
+//     <CartProvider
+//       mode="payment"
+//       cartMode="client-only"
+//       stripe={`${process.env.GATSBY_STRIPE_SECRET_KEY}`}
+//       successUrl="https://claybystacy.net/success"
+//       cancelUrl="https://claybystacy.net/"
+//       currency="USD"
+//       allowedCountries={["US", "GB", "CA"]}
+//       billingAddressCollection={true}
+//     >
+//       {element}
+//     </CartProvider>
+//   )
+// }
+// >>>>>>> parent of 35785f4 (Got rid of gatsby-plugin-use-shopping-cart, created a wrapper component instead)
