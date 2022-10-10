154b0e723ba13dcf0365f92849dff9b4ac5c72f6
Max Healy <maxhealy01@gmail.com>
2022-10-09 22:33:58 -0500

    Changing keys

--- src/utils/stripejs.js
+++ src/utils/stripejs.js
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js"
 let stripePromise
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.GATSBY_STRIPE_SECRET_KEY)
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLIC_KEY)
   }
 
   return stripePromise
