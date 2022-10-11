8dd30ea476decb48117aad4f8aa05cfdfc137433
Max Healy <maxhealy01@gmail.com>
2022-10-10 21:34:42 -0500

    Current working version. Async redirectToCheckout()

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,6 @@
 // const React = require("react")
 // const { CartProvider } = require("use-shopping-cart")
 
-// <<<<<<< HEAD
 // exports.wrapRootElement = ({ element }) => {
 //   return (
 //     <CartProvider
@@ -25,25 +24,3 @@
 //     </CartProvider>
 //   )
 // }
-// =======
-// import React from "react"
-// import { Provider } from "react-redux"
-// import { CartProvider } from "use-shopping-cart"
-
-// export const wrapRootElement = ({ element }) => {
-//   return (
-//     <CartProvider
-//       mode="payment"
-//       cartMode="client-only"
-//       stripe={`${process.env.GATSBY_STRIPE_SECRET_KEY}`}
-//       successUrl="https://claybystacy.net/success"
-//       cancelUrl="https://claybystacy.net/"
-//       currency="USD"
-//       allowedCountries={["US", "GB", "CA"]}
-//       billingAddressCollection={true}
-//     >
-//       {element}
-//     </CartProvider>
-//   )
-// }
-// >>>>>>> parent of 35785f4 (Got rid of gatsby-plugin-use-shopping-cart, created a wrapper component instead)
--- src/components/Cart/CartOverview.js
+++ src/components/Cart/CartOverview.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 
 import { useShoppingCart } from "use-shopping-cart"
-import { loadStripe } from "@stripe/stripe-js"
 
 import CartItems from "./CartItems"
 
@@ -13,10 +12,21 @@ const Cart = () => {
   const { formattedTotalPrice, redirectToCheckout, cartCount, clearCart } =
     useShoppingCart()
 
-  loadStripe(`${process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}`).then(res => {
-    console.log(res)
-  })
+  async function handleClick(event) {
+    event.preventDefault()
 
+    if (cartCount > 0) {
+      try {
+        setLoading(true)
+        const result = await redirectToCheckout()
+        if (result?.error) {
+          console.error(result)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
   return (
     <div>
       {/* This is where we'll render our cart */}
@@ -26,14 +36,7 @@ const Cart = () => {
       <p>Total: {formattedTotalPrice}</p>
 
       {/* Redirects the user to Stripe */}
-      <button
-        disabled={loading}
-        className="big-button"
-        onClick={() => {
-          setLoading(true)
-          redirectToCheckout()
-        }}
-      >
+      <button disabled={loading} className="big-button" onClick={handleClick}>
         {loading ? "Loading..." : "CHECKOUT"}
       </button>
       <button className="big-button" onClick={clearCart}>
