11820ed3a12bbeb82f45e2c19b766ae132bf4e3f
Max Healy <maxhealy01@gmail.com>
2022-11-01 19:38:39 -0500

    Changed create-session to remove validation and add the proper key.

--- netlify/functions/create-session.js
+++ netlify/functions/create-session.js
@@ -1,19 +1,17 @@
 // Serverless Function: Let's validate the product the user wants and create a Stripe Session.
-const stripe = require("stripe")(process.env.REACT_APP_STRIPE_API_SECRET)
-const { validateCartItems } = require("use-shopping-cart/src/serverUtil")
-const inventory = require("./data/products.json")
+const stripe = require("stripe")(process.env.GATSBY_STRIPE_SECRET_KEY)
 
 exports.handler = async event => {
   try {
     const productJSON = JSON.parse(event.body)
-    const line_items = validateCartItems(inventory, productJSON)
+    const line_items = productJSON
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       billing_address_collection: "auto",
       shipping_address_collection: {
         allowed_countries: ["US", "CA"],
       },
-      success_url: `${process.env.URL}/success.html`,
+      success_url: `${process.env.URL}/success`,
       cancel_url: process.env.URL,
       line_items,
     })
